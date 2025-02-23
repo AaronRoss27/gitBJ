c8ff408de75e434aa52a0c29e216c16ba2f4cb37
aaron <aaronross1031@gmail.com>
2025-02-23 16:22:06 -0500

    Completed For Now, No Ace Support or Splits

--- Main.java
+++ Main.java
@@ -1,40 +1,106 @@
 import java.util.*;
 import java.util.concurrent.*;
 
-public class Main{
 
-    public static int dealCards(int amount){
-        int total = 0;
-        for(int i = 1; i <= amount; i++){
-            int randomNum = ThreadLocalRandom.current().nextInt(4,11);
-            total += randomNum;
-        }
-        return total;
+
+public class Main {
+
+
+    //Deals a random card
+    static int deal(){
+        int card = ThreadLocalRandom.current().nextInt(4,11);
+        return card;
     }
 
-    public static boolean isBlackJack(int total){
-        if(total == 21){
-            return true;
-        }else{
-            return false;
+    //Deals 2 cards
+    static int initialDeal(){
+        int playerTotal = 0;
+        for(int i = 0; i< 2; i++){
+            playerTotal += deal();
         }
+        return playerTotal;
     }
 
-    public static boolean isBust(int total){
+    
+    //checks if the hand busted
+    static boolean isBusted(int total){
         if(total > 21){
             return true;
         }else{
             return false;
         }
     }
-
-
+    
+    //checks if the hand equals blackjack 
+    static boolean blackJack(int total){
+        if(total==21){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 
     public static void main(String[] args){
-        
-        int playerTotal = 0;
-        int dealerTotal = 0;
-        
+
+        Scanner scanner = new Scanner(System.in);
+        int playerTotal = initialDeal();
+        int dealerTotal = initialDeal();
+
+        while(true){
+            playerTotal = initialDeal();
+            dealerTotal = initialDeal();
+            System.out.println("New Game!");
+            System.out.println("Your total: " + playerTotal);
+            if(playerTotal == 21){
+                System.out.println("Blackjack!");
+                continue;
+            }else{
+                while(true){
+                    System.out.println("1 for Hit or 2 for Stand");
+                    int choice = scanner.nextInt();
+                    if(choice==1){ //hit
+                        playerTotal += deal();
+                        System.out.println("Total: " + playerTotal);
+                        if(blackJack(playerTotal)){
+                            System.out.println("Blackjack!");
+                            break;
+                        }else if(isBusted(playerTotal)){
+                            System.out.println("BUSTED!");
+                            break;
+                        }else{
+                            continue;
+                        }
+
+                    }else{//Stand
+                        System.out.println("Dealers hand: " + dealerTotal);
+                        if(dealerTotal == 21){
+                            System.out.println("Blackjack! You lost!");
+                            break;
+                        }else if(playerTotal == dealerTotal){
+                            System.out.println("Push!");
+                            break;
+                        }else{
+                            while(dealerTotal < playerTotal && dealerTotal < 21){
+                                dealerTotal += deal();
+                                System.out.println("Dealer Hits!");
+                                System.out.println("Dealer Total: " + dealerTotal);
+                                if(dealerTotal > 21){
+                                    System.out.println("Dealer BUSTED! You win!");
+                                    break;
+                                }else if(blackJack(dealerTotal)){
+                                    System.out.println("Blackjack! You lost!");
+                                    break;
+                                }else if(dealerTotal > playerTotal && !blackJack(dealerTotal) && !isBusted(dealerTotal)){
+                                    System.out.println("You lost!");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    break; //leaves Hit or Stand
+                }
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is a simple Black Jack game made with Java
+Previously made without Git and then just copy and pasted over 
